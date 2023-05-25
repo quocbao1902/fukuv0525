75a2f6849b64de7005251a5c80b2b2193f5f6c66
Nguyen Quoc Bao <130636520+quocbao1902@users.noreply.github.com>
2023-05-25 13:38:23 +0900

    fukuv0525

--- fukuv0525/Form1.cs
+++ fukuv0525/Form1.cs
@@ -16,5 +16,25 @@ namespace fukuv0525
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 15;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 15;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 15;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 15;
+        }
     }
 }
